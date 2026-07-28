105d694c4280293e32fe86ea84336844469ebce4
Alaa <alaar@A_LAPTOP>
2026-07-28 18:10:33 +0400

    added entities to emloyee class

--- EFcoreProject/Models/Emloyee.cs
+++ EFcoreProject/Models/Emloyee.cs
@@ -6,5 +6,10 @@ namespace EFcoreProject.Models
 {
     public class Emloyee
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int age { get; set; }
+        public double Salary { get; set; }
+
     }
 }
