3e9a7021ff3c7261c33ed3a76318baead5f7f8fb
Alaa <alaar@A_LAPTOP>
2026-07-28 20:38:47 +0400

    added a new employee

--- EFcoreProject/Program.cs
+++ EFcoreProject/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace EFcoreProject
+﻿using System;
+using EFcoreProject.Models; //importing the models namespace to use the Employee and Department classes in the Program class.
+
+namespace EFcoreProject
 {
     internal class Program
     {
@@ -6,8 +9,15 @@
         {
             ProjectContext context = new ProjectContext(); //creating an instance of the ProjectContext class to access the database
 
-            //adding a new employee to the database
-
+            //adding an emloyee
+            Employee e1 = new Employee();
+            e1.EmloyeeName = "Ahmed";
+            e1.EmployeeAge = 30;
+            e1.EmployeeSalary = 50000.0;
+            e1.SSN = 123456789;
+            //we dont add the EmployeeId because it is an identity column and will be generated automatically by the database.
+            context.employees.Add(e1);
+            context.SaveChanges();
         }
     }
 }
