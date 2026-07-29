8d72fb4ef9d5573d68377ca0d066789dd5206aaf
Alaa <alaar@A_LAPTOP>
2026-07-29 15:58:26 +0400

    added sttributes for product and sat the CategoryID as foreign key

--- ECommerceDatabase/Models/Product.cs
+++ ECommerceDatabase/Models/Product.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace ECommerceDatabase.Models
 {
-    internal class Product
+    public class Product
     {
+        public int ProductId { get; set; } //auto-incremented primary key
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+        public int CategoryId { get; set; } //foreign key
     }
 }
