e42000e8203f3137211c11f1e8b1aba06f610b01
Alaa <alaar@A_LAPTOP>
2026-07-29 16:30:25 +0400

    added the code for the AppDbContext, registering the models and configuiring the database

--- ECommerceDatabase/AppDBContext.cs
+++ ECommerceDatabase/AppDBContext.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using ECommerceDatabase.Models;
 
 namespace ECommerceDatabase
 {
-    internal class AppDBContext
+    public class AppDBContext : DbContext
     {
+        //register the models with the database context
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+
+        //2 . Configure the database connection string
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=A_Laptop\\SQLEXPRESS;Database=ECommerceDatabase;Trusted_Connection=True;TrustServerCertificate=true;");
+        }
     }
 }
