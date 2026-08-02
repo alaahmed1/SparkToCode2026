5e4d721b32363bfe7df703186070a2e5ed802902
Alaa <alaar@A_LAPTOP>
2026-08-02 09:46:43 +0400

    added the Project Context file and connected to create a database and edited the appsetting.json

---
+++ WebAPIProject/WebAPIProjectContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIProject.Models;
+namespace WebAPIProject
+{
+    public class WebAPIProjectContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+        public WebAPIProjectContex (DbContextOptions<WebAPIProjectContext> options) : base(options)
+        {
+        }
+    }
+}
